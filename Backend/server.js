39ba44c7f70d587216de5dd012d79d3747f70136
pushthroughpain4@gmail.com <pushthroughpain4@gmail.com>
2025-09-04 00:58:37 +0530

    .

--- Backend/server.js
+++ Backend/server.js
@@ -14,7 +14,13 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // or restrict to your frontend domain
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(clerkMiddleware());
